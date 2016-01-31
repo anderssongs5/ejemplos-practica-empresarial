07d3d659b8e53ff0f5b98e688cd091244d299936
Andersson García Sotelo <anderssongarciasotelo@gmail.com>
2016-01-30 22:03:47 -0500

    Adición de clase faltante.

---
+++ src/java/main/co/edu/udea/practicaempresarial/rx/ObservableFrom.java
@@ -0,0 +1,47 @@
+package co.edu.udea.practicaempresarial.rx;
+
+import java.io.IOException;
+import java.nio.file.DirectoryStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.List;
+
+import rx.Observable;
+
+public class ObservableFrom {
+
+    private static void crearDesdeLista() {
+        List<String> colors = Arrays.asList("Azul", "Rojo", "Verde", "Amarillo", "Naranja", "Gris", "Púrpura");
+        Observable<String> observableColors = Observable.from(colors);
+
+        observableColors.subscribe(System.out::println);
+    }
+
+    private static void crearDesdePath() {
+        Path resources = Paths.get("src", "java", "resources");
+
+        try {
+            DirectoryStream<Path> directoryStream = Files.newDirectoryStream(resources);
+            Observable<Path> directoryObservable = Observable.from(directoryStream);
+            directoryObservable.subscribe(System.out::println);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void crearDesdeArreglo() {
+        Observable<Integer> numbersObservable = Observable.from(new Integer[] { 3, 5, 8 });
+
+        numbersObservable.subscribe(System.out::println);
+    }
+
+    public static void correr() {
+        crearDesdeLista();
+
+        crearDesdePath();
+
+        crearDesdeArreglo();
+    }
+}
